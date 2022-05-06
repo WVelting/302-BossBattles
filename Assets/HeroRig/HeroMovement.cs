1a0bcf8205e205f909873ec7d0a0ba7e4470737b
William Velting <wvelting@gmail.com>
2022-05-06 17:21:41 -0400

    Animated Hero - Part One
    +Imported GunGuy fbx
    +Added HeroMovement Script
    +Added PointAt Script
    +Added HeroMAT
    +Added GroundMAT
    +Added HeroAnimatorStateMachine

---
+++ Assets/HeroRig/HeroMovement.cs
@@ -0,0 +1,164 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent (typeof(Animator))]
+public class HeroMovement : MonoBehaviour
+{
+
+    enum Mode{
+        Idle,
+        Walk,
+        InAir
+
+    }
+
+
+    private CharacterController pawn;
+    private Mode mode = Mode.Idle;
+    private Vector3 input;
+    private float walkTime;
+    private Quaternion targetRot;
+    ///<summary>The current vertical velocity in meters/second</summary>
+    
+
+    public FootRaycast footLeft;
+    public FootRaycast footRight;
+    private Camera cam;
+    public float footSeparateScalar = -.2f;
+    public float walkSpreadY = .4f;
+    public float walkSpreadZ = .8f;
+    public float walkFootSpeed = 4;
+
+    public float walkSpeed = 5;
+    public float velocityY = 0;
+    public float gravity = 20;
+    public float jumpImpulse = 10;
+
+    void Start()
+    {
+        pawn = GetComponent<CharacterController>();   
+        cam = Camera.main; 
+    }
+
+    void Update()
+    {
+        float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
+
+        Vector3 camForward = cam.transform.forward;
+        camForward.y = 0;
+        camForward.Normalize();
+
+        Vector3 camRight = Vector3.Cross(Vector3.up, camForward);
+
+        input = camForward * v + camRight * h;
+        if(input.sqrMagnitude > 1) input.Normalize();
+
+
+        float threshold = .1f;
+        //set movement mode based on movement input:
+        mode = (input.sqrMagnitude > threshold*threshold) ? Mode.Walk : Mode.Idle;
+
+        if(mode == Mode.Walk) targetRot = Quaternion.LookRotation(input, Vector3.up);
+
+        if(pawn.isGrounded)
+        {
+            if(Input.GetButtonDown("Jump"))
+            {
+                velocityY = -jumpImpulse;
+            }
+        }
+
+        velocityY += gravity * Time.deltaTime;
+        
+        pawn.Move((input * walkSpeed + Vector3.down * velocityY) * Time.deltaTime);
+
+        if(pawn.isGrounded) velocityY = 0;
+        else mode = Mode.InAir;
+        // animate the feet
+        Animate();
+    }
+
+    void Animate()
+    {
+        if(mode == Mode.Walk) 
+        {
+            targetRot = Quaternion.LookRotation(input, Vector3.up);
+            float eulerY = targetRot.eulerAngles.y;
+            eulerY += 90;
+            targetRot = Quaternion.Euler(targetRot.eulerAngles.x, eulerY, targetRot.eulerAngles.z);
+            transform.rotation = AniMath.Ease(transform.rotation, targetRot, .01f);
+        }
+        switch(mode)
+        {
+            case Mode.Idle:
+                AnimateIdle();
+                break;
+            case Mode.Walk:
+                AnimateWalk();
+                break;
+            case Mode.InAir:
+                AnimateInAir();
+                break;
+        }
+        
+
+    }
+
+    private void AnimateInAir()
+    {
+        //TODO:
+
+        //lift legs
+        //lift hands
+        //adjust spikes/hair
+        //use vertical velocity
+    }
+
+    void AnimateIdle()
+    {
+        footLeft.SendHomePosition();
+        footRight.SendHomePosition();
+        walkTime = 0;
+    }
+
+    delegate void MoveFoot(float time, FootRaycast foot);
+
+    void AnimateWalk()
+    {
+        //DRY - Don't Repeat Yourself
+        
+        MoveFoot moveFoot = (t, foot)=>{ 
+        
+            float y = Mathf.Cos(t) * walkSpreadY; //vertical movement
+            float lateral = Mathf.Sin(t) * walkSpreadZ; //forward/backward
+            
+            Vector3 localDir = foot.transform.parent.InverseTransformDirection(input);
+            Vector3 separateDir = Vector3.Cross(Vector3.up, localDir);
+
+
+            float z = lateral * localDir.z;
+            float x = lateral * localDir.x;
+
+            float alignment = Mathf.Abs(Vector3.Dot(localDir, Vector3.forward));
+            
+            
+
+
+
+            if(y < 0) y = 0;
+
+
+            if(foot)foot.SetOffsetPosition(new Vector3(x,y,z), footSeparateScalar * alignment);
+        };
+
+        walkTime += Time.deltaTime * input.sqrMagnitude * walkFootSpeed;
+
+
+        
+        moveFoot.Invoke(walkTime, footLeft);
+        moveFoot.Invoke(walkTime+Mathf.PI, footRight);
+    }
+}
---
+++ Assets/HeroRig/PointAt.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum Axis
+{
+    Forward,
+    Backward,
+    Right,
+    Left,
+    Up,
+    Down
+}
+public class PointAt : MonoBehaviour
+{
+
+    public Transform target;
+    
+    public Axis aimOrientation = Axis.Backward;
+    
+    public bool lockAxisX = false; 
+    public bool lockAxisY = false; 
+    public bool lockAxisZ = false; 
+
+    private Quaternion startRotation;
+    private Quaternion goalRotation;
+    private Vector3 fromVector = new Vector3();
+    
+    void Start()
+    {
+        startRotation = transform.localRotation;
+
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButton(1)) TurnTowardsTarget();
+    }
+
+    private void TurnTowardsTarget()
+    {
+        if(target != null)
+        {
+            Vector3 vToTarget = target.position - transform.position;
+            
+            vToTarget.Normalize();
+
+            Quaternion worldRot = Quaternion.LookRotation(vToTarget, Vector3.up);
+            Quaternion localRot = worldRot;
+
+            if(transform.parent){
+
+                //convert to local-space:
+                localRot = Quaternion.Inverse(transform.parent.rotation) * worldRot;
+            }
+
+            Vector3 euler = localRot.eulerAngles;
+            if(lockAxisX) euler.x = startRotation.eulerAngles.x;
+            if(lockAxisY) euler.y = startRotation.eulerAngles.y;
+            if(lockAxisZ) euler.z = startRotation.eulerAngles.z;
+            euler.y+=90;
+            localRot.eulerAngles = euler;
+
+            goalRotation = localRot;
+        }
+        else goalRotation = startRotation;
+
+        transform.localRotation = AniMath.Ease(transform.localRotation, goalRotation, .001f);
+    }
+}
